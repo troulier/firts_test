ff64ef8c026fa6624e2c0b927b9a0515a264eff1
thibault <thibault.roulier@gmail.com>
2019-04-28 10:17:51 +0200

    initialisation projet menu restau

---
+++ src/com/testGithub/Main.java
@@ -0,0 +1,32 @@
+package com.testGithub;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int trois = 3;
+
+        System.out.println("Hello OpenClassrooms");
+        System.out.println("fludublu");
+        System.out.println(trois);
+
+        for(int i = 1; i <= 10; i++)
+        {
+
+            trois = trois + i;
+        }
+        System.out.println(trois);
+
+
+        for(int i = 1; i <= 10; i++)
+        {
+
+            trois += i * i;
+            System.out.println(trois);
+        }
+
+
+    }
+}
+
+
